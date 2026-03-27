0ec9d2c537ec36f8aed3d718e85bfcdc185bba27
Guilherme <guilhermede.abreu@hotmail.com>
2026-03-26 23:29:40 -0300

    favicon admin

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'LinhaCash',
   description: 'Análise de props da NBA',
+  icons: {
+    icon: '/logo.png',
+    apple: '/logo.png',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
